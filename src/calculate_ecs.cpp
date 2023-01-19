8cf256b52edccbab7c91ba9a306aa3a1f95444d4
Andi Munteanu <munteanu_andi98@yahoo.com>
2023-01-19 09:22:33 +0000

    [fix] Fix the cpp code for contingency table and ECS.

--- src/calculate_ecs.cpp
+++ src/calculate_ecs.cpp
@@ -4,11 +4,17 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 IntegerMatrix myContTable(IntegerVector &a, IntegerVector &b) {
-    int nClustersA = max(a) + 1, nClustersB = max(b) + 1, n = a.size();
+	int minim_mb1 = min(a), minim_mb2 = min(b);
+	int maxim_mb1 = max(a), maxim_mb2 = max(b);
+    int nClustersA = maxim_mb1 - minim_mb1 + 1;
+	int nClustersB = maxim_mb2 - minim_mb2 + 1, n = a.size();
+
+	// Rprintf("The value of foo is %i %i %i %i %i %i\n", minim_mb1, minim_mb2, maxim_mb1, maxim_mb2, nClustersA, nClustersB);
+
     IntegerMatrix result(nClustersA, nClustersB);
-    
+
     for(int i = 0; i < n; i++) {
-        result(a(i), b(i))++;
+        result(a(i) - minim_mb1, b(i) - minim_mb2)++;
     }
 
 	return(result);
@@ -18,16 +24,11 @@ IntegerMatrix myContTable(IntegerVector &a, IntegerVector &b) {
 // [[Rcpp::export]]
 NumericVector disjointECS(IntegerVector mb1, IntegerVector mb2) {
 	int minim_mb1 = min(mb1), minim_mb2 = min(mb2);
-	IntegerVector copy_mb1 = mb1;
-	if (minim_mb1) {
-		copy_mb1 = copy_mb1 - minim_mb1;
-	}
-	IntegerVector copy_mb2 = mb2;
-	if (minim_mb2) {
-		copy_mb2 = copy_mb2 - minim_mb2;
-	}
-	IntegerMatrix contTable = myContTable(copy_mb1, copy_mb2);
-    int n = copy_mb1.size(), nClusters1 = contTable.nrow(), nClusters2 = contTable.ncol();
+	int cluster1, cluster2;
+	double c1, c2, pointECS, intersectSize;
+	
+	IntegerMatrix contTable = myContTable(mb1, mb2);
+    int n = mb1.size(), nClusters1 = contTable.nrow(), nClusters2 = contTable.ncol();
 
 	NumericMatrix uniqueECSvals(nClusters1, nClusters2);
 	NumericVector ecsScore(n);
@@ -35,15 +36,18 @@ NumericVector disjointECS(IntegerVector mb1, IntegerVector mb2) {
 	uniqueECSvals.fill(-1);
 
 	for (int i = 0; i < n; i++) {
-		int cluster1 = copy_mb1(i), cluster2 = copy_mb2(i);
+		cluster1 = mb1(i) - minim_mb1;
+		cluster2 = mb2(i) - minim_mb2;
 
 		if (uniqueECSvals(cluster1, cluster2) != -1) {
 			ecsScore(i) = uniqueECSvals(cluster1, cluster2);
 			continue;
 		}
 
-		double c1 = clustersSizes1(cluster1), c2 = clusterSizes2(cluster2), intersectSize = contTable(cluster1, cluster2);
-		double pointECS = (1 / c1 - 1 / c2) * intersectSize;
+		c1 = clustersSizes1(cluster1);
+		c2 = clusterSizes2(cluster2);
+		intersectSize = contTable(cluster1, cluster2);
+		pointECS = (1 / c1 - 1 / c2) * intersectSize;
 		if (pointECS < 0) {
 			pointECS *= -1;
 		}
