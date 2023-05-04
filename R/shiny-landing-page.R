89a9272752222d62cb97de5ebd3d7efa22020ff2
Andi Munteanu <munteanu_andi98@yahoo.com>
2023-05-04 23:12:11 +0100

    [chore] Add variables that count the number of times a button is pressed

--- R/shiny-landing-page.R
+++ R/shiny-landing-page.R
@@ -139,8 +139,11 @@ server_landing_page <- function(id, height_ratio, dimension) {
         add_env_variable("metadata", mdt$metadata)
         add_env_variable("metadata_colors", mdt$metadata_colors)
         add_env_variable("metadata_unique", mdt$metadata_unique)
+        add_env_variable("enable_markers_button", shiny::reactiveVal(-1))
+        add_env_variable("find_markers_button", shiny::reactiveVal(-1))
         rm(mdt)
         print(paste(Sys.time(), "landing - finished loading"))
+        gc()
     }
     
   )
