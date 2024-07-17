581f0564b5e9405c2259065a9cb361908d2bd217
AndiMunteanu <munteanu_andi98@yahoo.com>
2024-07-17 09:10:52 +0300

    [fix] Fix the way the options are printed. Also provide more flexibility in summarising the ECC.

--- R/utils.R
+++ R/utils.R
@@ -75,23 +75,23 @@ process_clustering_arguments <- function(clustering_arguments, clustering_method
 }
 
 #' Extract config-specific clusters from a ClustAssess object
-#' 
+#'
 #' @description Given the output of the `automatic_stability_assessment`
-#' function, extract the clusters that are specific to a particular 
+#' function, extract the clusters that are specific to a particular
 #' configuration of feature type, feature size, clustering method and,
 #' optionally, the number of clusters.
-#' 
+#'
 #' @param clustassess_object Output of the `automatic_stability_assessment`.
 #' @param feature_type Type of feature used for dimensionality reduction.
 #' @param feature_size Size of the feature set used for clustering.
 #' @param clustering_method Clustering method used.
 #' @param nclusters Number of clusters to extract. If NULL, all clusters are
-#' returned. 
-#' 
+#' returned.
+#'
 #' @return A list of clusters that are specific to the given configuration.
 #' Each number of cluster will contain the list of partitions with that
 #' specific k and the ECC value indicating the overall stability of k.
-#' 
+#'
 #' @export
 get_clusters_from_clustassess_object <- function(clustassess_object,
                                                  feature_type,
@@ -106,13 +106,15 @@ get_clusters_from_clustassess_object <- function(clustassess_object,
 
     clustassess_object <- clustassess_object[[feature_type]]
     if (!(feature_size %in% names(clustassess_object))) {
-        available_options <- setdiff(names(clustassess_object), "feature_list")
+        available_options <- paste(setdiff(names(clustassess_object), "feature_list"), collapse = ", ")
         stop(glue::glue("Feature size not found in clustassess object.\nAvailable options: {available_options}"))
     }
 
+    feature_size <- as.character(feature_size)
+
     clustassess_object <- clustassess_object[[feature_size]]$clustering_stability$split_by_k
     if (!(clustering_method %in% names(clustassess_object))) {
-        available_options <- names(clustassess_object)
+        available_options <- paste(names(clustassess_object), collapse = ", ")
         stop(glue::glue("Clustering method not found in clustassess object.\nAvailable options: {available_options}"))
     }
 
@@ -126,7 +128,7 @@ get_clusters_from_clustassess_object <- function(clustassess_object,
     nclusters <- intersect(nclusters, names(clustassess_object))
 
     if (length(nclusters) == 0) {
-        available_options <- names(clustassess_object)
+        available_options <- paste(names(clustassess_object), collapse = ", ")
         stop(glue::glue("Number of clusters not found in clustassess object.\nAvailable options: {available_options}"))
     }
 
@@ -134,32 +136,36 @@ get_clusters_from_clustassess_object <- function(clustassess_object,
 }
 
 #' Choose stable clusters based on ECC and frequency
-#' 
+#'
 #' @description Filter the list of clusters obtained by the automatic
 #' ClustAssess pipeline using the ECC and frequency thresholds. The ECC
 #' threshold is meant to filter out the partitions that are highly sensitive
 #' to the change of the random seed, while the purpose of the frequency
 #' threshold is to assure a statistical significance of the inferred stability.
-#' 
+#'
 #' @param clusters_list List of clusters obtained from the
 #' `get_clusters_from_clustassess_object` function.
 #' @param ecc_threshold Minimum ECC value to consider a cluster as stable.
 #' Default is 0.9.
 #' @param freq_threshold Minimum total frequency of the partitions to consider.
-#' Default is 30. 
-#' 
+#' Default is 30.
+#' @param summary_function Function to summarize the ECC values. Default
+#' is `mean`. To match the results from the ClustAssess Shiny App, use
+#' `median`.
+#'
 #' @return A list of stable clusters that satisfy the ECC and frequency.
-#' 
+#'
 #' @export
 choose_stable_clusters <- function(clusters_list,
                                    ecc_threshold = 0.9,
-                                   freq_threshold = 30) {
+                                   freq_threshold = 30,
+                                   summary_function = mean) {
 
     if (!all(c("ecc", "partitions") %in% names(clusters_list[[1]]))) {
         stop("Invalid clusters list. Please run `get_clusters_from_clustassess_object` first on the ClustAssess object.")
     }
 
-    ecc_vals <- sapply(clusters_list, function(x) mean(x$ecc))
+    ecc_vals <- sapply(clusters_list, function(x) summary_function(x$ecc))
     total_freq <- sapply(clusters_list, function(x) {
         sum(sapply(x$partitions, function(y) y$freq))
     })
@@ -168,7 +174,7 @@ choose_stable_clusters <- function(clusters_list,
     index_freq <- which(total_freq >= freq_threshold)
 
     index_chosen <- intersect(index_ecc, index_freq)
-    
+   
     if (length(index_chosen) == 0) {
         stop("No stable clusters found. Consider lowering the `ecc_threshold` or `freq_threshold`.")
     }
